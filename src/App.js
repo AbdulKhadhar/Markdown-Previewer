503836b170134fc87e16ca042b841a8157068454
AbdulKhadhar <abdulkhadhar13@gmail.com>
2022-03-08 18:46:40 +0530

    Markdown Previewe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Badge from "react-bootstrap/Badge";
+import { marked } from 'marked'
+//let marked = require("marked");
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ""
+    }
+  }
+
+  UpdateMarkdown(markdown) {
+    this.setState({ markdown })
+  }
+  render() {
+
+    var StyleIn = {
+      width: "400px",
+      height: "50vh",
+      padding: "10px",
+      marginLeft: "auto",
+      marginRight: "auto"
+    };
+    var StyleOut = {
+      width: "400px",
+      height: "50vh",
+      padding: "10px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      backgroundColor: "rgba(0,25,0,0.2"
+    };
+    return (
+      <div className='App'>
+        <div className='container'>
+          <div className='row mt-4'>
+            <div className='col text-center'>
+
+              <h1><Badge className='text-align-center' variant='light'>
+                Markdown Previewer
+              </Badge></h1>
+
+              <div className='row mt-4'>
+                <div className='col md-6'>
+                  <div className='col text-center'>
+                    <h1>
+                      <Badge className='text-align-center' varient='light'>Input</Badge>
+                    </h1></div>
+                  <textarea id='editor' style={StyleIn} value={this.state.markdown} onChange={(e) => { this.UpdateMarkdown(e.target.value) }}></textarea>
+                </div>
+                <div className='col md-6'>
+                  <div className='col text-center'>
+                    <h1>
+                      <Badge className='text-align-center' varient='light'>Preview</Badge>
+                    </h1></div>
+                  <div id='preview' style={StyleOut} dangerouslySetInnerHTML ={{__html:marked(this.state.markdown)}}></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
-
-export default App;
